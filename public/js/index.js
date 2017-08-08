e40a382a35a28d48e0d28d20e70fc5ba385819d6
alimohamed-moussa <alimohamed.moussa@gmail.com>
2017-08-08 04:26:13 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,16 +3,12 @@ var socket = io();
 socket.on('connect', function () {
   console.log('connected to server');
 
-  socket.emit('createMessage', {
-    from: 'Ali',
-    text: 'Hey this is Ali'
-  });
 });
 
 socket.on('disconnect', function () {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage',function (message){
+socket.on('newMessage', function (message) {
   console.log('New message', message);
 });
